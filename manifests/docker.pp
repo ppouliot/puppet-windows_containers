89a0a73669300ed16f4be525f8da68789868b07b
primeministerp <primeministerpete@gmail.com>
2016-04-05 12:52:01 -0400

    placeholder for docker on windows

---
+++ manifests/docker.pp
@@ -0,0 +1,8 @@
+# == Class: windows_containers::docker
+#   Translation of steps to install docker on windows here:
+#   https://msdn.microsoft.com/en-us/virtualization/windowscontainers/deployment/docker_windows
+
+class windows_containers::docker {
+
+}
+
